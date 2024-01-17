58d4334b063032c1f4837de11093a3f2dcfd35ff
ResatKavcı <kavciresat@gmail.com>
2024-01-18 02:40:39 +0300

    Test Automation

--- src/test/java/API_Test_Automation.java
+++ src/test/java/API_Test_Automation.java
@@ -11,6 +11,7 @@ import static org.hamcrest.Matchers.*;
 
 public class API_Test_Automation {
 
+    // BDD stratejiyle Given,When,Then
     @Test
     public void ListUsers() {
         when()
@@ -20,17 +21,27 @@ public class API_Test_Automation {
                 .time(lessThan(1000L));
     }
 
+    //Kodsal olarak API test süreci
     @Test
     public void SingleUser() {
+        //RestAssured kütüphanesini kullanarak URL'e bir GET isteği gönderir ve yanıtı response değişkenine atar.
         Response response = get("https://reqres.in/api/users/2");
 
+        // HTTP yanıtını bir dize olarak yazdırır.
         System.out.println("Response :" + response.asString());
+        //HTTP yanıtının durum kodunu yazdırır.
         System.out.println("Status Code :" + response.getStatusCode());
+        //HTTP yanıtının gövdesini bir dize olarak yazdırır.
         System.out.println("Body :" + response.getBody().asString());
+        //İsteğin tamamlanma süresini (milisaniye cinsinden) yazdırır.
         System.out.println("Time taken :" + response.getTime());
+        //content-type" adlı HTTP başlığının değerini yazdırır.
         System.out.println("Header :" + response.getHeader("content-type"));
 
+        //HTTP yanıtının durum kodunu bir değişkene atar.
         int statusCode = response.getStatusCode();
+        //HTTP yanıtının durum kodunun 200 olup olmadığını kontrol eder.Başarılı bir HTTP GET isteğini temsil eder.
+        // 200 den farklı bir expected  değer girildiğinde konsola Expected ve Actual(beklenen/gerçek) response bilgis döner
         Assert.assertEquals(statusCode, 200);
     }
 
@@ -47,12 +58,16 @@ public class API_Test_Automation {
 
     @Test
     public void Create() {
+        //API endpoint'inin base URI'sini belirler. post isteğinde baseURL belirttiğimiz için sonrasında ki endpointi yazmamız yeterli olacaktır
         baseURI = "https://reqres.in/api";
 
+        //Map, anahtar (key) ve değer (value) çiftlerini içeren bir koleksiyon tipidir. Kalıp bir ifadedir
         Map<String, Object> postData = new HashMap<String, Object>();
+        //Gönderilecek veriyi belirttik
         postData.put("name", "ResatKavci");
         postData.put("job", "APITestAutomation");
 
+        //JSON nesnesi olan request oluşturulur. Bu, POST isteğinin gövdesini temsil eder.
         JSONObject request = new JSONObject(postData);
 
                 given().
@@ -60,8 +75,10 @@ public class API_Test_Automation {
                 when().
                         post("/users").
                 then().
-                        statusCode(201).
+                        statusCode(201). //Yanıt kontrolü yapılır. statusCode(201) ile yanıtın durum kodunun 201 (Created) olup olmadığı kontrol edilir. log().all() ile yanıtın tamamı konsola yazdırılır.
                         log().all();
+
+                //SON isteği konsola yazdırılır.
         System.out.println(request.toJSONString());
 
     }
